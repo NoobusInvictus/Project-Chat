22e61d9211de6d22086bd0c56e78af349ba2a2bd
NoobusInvictus <71375467+NoobusInvictus@users.noreply.github.com>
2023-08-14 17:42:19 +0200

    Add files via upload

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,21 @@
-const ChatsPage = () => {
-  return <>chats...</>;
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "20a7c626-52df-4d70-915e-e3cb47b2197c",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
 };
 export default ChatsPage;
 
